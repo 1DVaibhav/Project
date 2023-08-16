a8d7121815e1f6d1a2e5636ab0b59138c7a5ced3
Vaibhav <vdd8010@gmail.com>
2023-08-16 17:46:37 +0530

    Add README.md.

---
+++ JokesWebApp/Models/joke.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace JokesWebApp.Models
+{
+    public class joke
+    {
+        public int id { get; set; }
+
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public joke()
+        {
+                
+        }
+    }
+}
